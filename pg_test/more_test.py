71c10cc394fa57f90f8b8d93683c90bcb96678b5
unknown <lm175767@gmail.com>
2026-01-12 21:05:58 -0300

    #2 test allure

--- pg_test/more_test.py
+++ pg_test/more_test.py
@@ -50,7 +50,7 @@ def test_ct02_senha_invalida(page):
 def test_ct03_usuario_invalido(page):
     acessar_login(page)
     page.wait_for_timeout(4500)
-    preencher_login(page, "ee", VALID_PASS)
+    preencher_login(page, "", VALID_PASS)
     page.wait_for_timeout(4500)
 
     visible = page.locator("text=Invalid credentials").is_visible()
